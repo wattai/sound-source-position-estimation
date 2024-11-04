a213173f978f2e12aca432280a621e3b58bab5ff
wattai <evamark.wattai@gmail.com>
2024-11-04 22:26:42 +0900

    wip: reimplementing MUISC method

--- src/sse/music_v2.py
+++ src/sse/music_v2.py
@@ -12,7 +12,6 @@ class MusicSoundSourceLocator(SoundSourceLocatorBase):
         self,
         fs: float,
         d: float,
-        c: float = 340.0,
         N_theta: int = 180,
     ):
         """Implementation of MUSIC method.
@@ -20,16 +19,16 @@ class MusicSoundSourceLocator(SoundSourceLocatorBase):
         Args:
             fs: Sampling frequency [Hz].
             d: distance between mics.
-            c: sound speed. Defaults to 340.0.
             N_theta (int, optional): _description_. Defaults to 180.
         """
 
         self.fs = fs  # sampling frequency [Hz].
-        self.T = 1.0 / fs  # sampling Term [sec].
+        self.Ts = 1.0 / fs  # sampling Term [sec].
         self.d = d  # mic. distance [m].
         self.k = None  # data index series [point].
         self.L = None  # data length [point].
-        self.c = c  # sound speed [m/sec].
+        self.c = 340
+
         self.N_theta = N_theta
         self.N_ch = None
 
@@ -40,59 +39,133 @@ class MusicSoundSourceLocator(SoundSourceLocatorBase):
         self,
         X: np.ndarray,
         y: np.ndarray | None = None,
+        num_sources: int = 1,
     ) -> "MusicSoundSourceLocator":
         # x: input sound signal
         # x.shape = (sample, N_ch)
 
-        self.L = len(X) // 1
+        self.L = len(X)
         self.k = np.arange(self.L // 2)
         self.N_ch = X.shape[1]
 
-        winfunc = np.hanning(len(X)).reshape(-1, 1)
-
-        X_freq = np.fft.fft(winfunc * X, axis=0)[: self.L // 2, :]
-        self.R = (
-            X_freq.conj().reshape(-1, self.N_ch, 1) @ X_freq.reshape(-1, 1, self.N_ch)
-        ) / (X_freq.shape[0] / self.fs)
-
-        # self.eigval, self.eigvec = np.linalg.eig(self.R)
+        self.window_width = 512
+        print("X.shape", X.shape)
+        X_spectrogram = stft(X, window_width=self.window_width)  # [time, freq, ch]
+        print("X_spectrogram.shape", X_spectrogram.shape)
+        self.R = X_spectrogram.transpose(1, 2, 0) @ X_spectrogram.conj().transpose(
+            1, 0, 2
+        )
         self.eigvec, self.eigval, _ = np.linalg.svd(self.R)
-
-        self.minvec = self.eigvec[:, :, -1].reshape(-1, self.N_ch, 1)
-
-        self.thetas = np.linspace(-np.pi / 2, np.pi / 2, self.N_theta)
+        # self.eigval, self.eigvec = np.linalg.eig(self.R)
+        print("self.eigvec.shape", self.eigvec.shape)
+        self.minvec = self.eigvec[:, :, num_sources:].reshape(
+            -1, self.N_ch, self.N_ch - num_sources
+        )
+        self.thetas = np.linspace(0, np.pi, self.N_theta)  # -90deg ~ +90deg
+        # self.thetas = np.linspace(-np.pi / 2, np.pi / 2, self.N_theta)  # -90deg ~ +90deg
         self.S = self._calc_s_music(self.thetas)
-        return self._theta_hat()
+        print("S.shape", self.S.shape)
+
+        # import matplotlib.pyplot as plt
+        # plt.pcolor(np.abs(self.S))
+        # plt.show()
+        # plt.plot(np.abs(self.S.mean(1)))
+        # plt.show()
+
+        return np.rad2deg(self.thetas[np.abs(self.S)[:, :].mean(1).argmax()])
 
     def _theta_hat(self):
         return np.rad2deg(self.thetas[(np.abs(self.S) ** 2).mean(0).argmax()])
 
-    def _calc_s_music(self, thetas):
-        return np.array(
-            [
-                np.sum(self._calc_alpha(theta).conj() * self._calc_alpha(theta), axis=0)
-                / np.abs(
-                    self.minvec.conj().transpose(0, 2, 1)
-                    @ self._calc_alpha(theta).transpose(1, 0).reshape(-1, self.N_ch, 1)
-                ).squeeze()
-                ** 2
-                for theta in thetas
-            ]
-        ).T * np.sqrt(
-            self.eigval[:, 0][:, None]
-        )  # / (self.eigval[:, -1][:, None]+1e-8) )
-
-    def _calc_alpha(self, theta):
-        return np.array(
-            [
-                np.ones(len(self.k)),
-                np.exp(
-                    2j
-                    * np.pi
-                    * self.d
-                    * np.sin(theta)
-                    / (self.c * self.T)
-                    * ((self.k) / (len(self.k) * 2))
-                ),
-            ]
+    def _calc_s_music(self, thetas: list[float]):
+        freqs = np.fft.fftfreq(n=self.window_width, d=self.Ts)
+        print("freqs", freqs)
+        print(
+            "self._calc_alpha2(theta, freqs).shape",
+            self._calc_alpha2(thetas[0], freqs).shape,
         )
+
+        outs = []
+        for theta in thetas:
+            a = self._calc_alpha2(theta, freqs=freqs).reshape(-1, 1, self.N_ch)
+            print("a.shape", a.shape)
+            print("self.minvec.shape", self.minvec.shape)
+            # print("np.linalg.vector_norm(a, axis=1, ord=2).shape", np.linalg.vector_norm(a, axis=1, ord=2).shape)
+            upper = np.abs((a.conj() @ self.minvec))[:, 0, :] ** 2
+            lower = np.linalg.vector_norm(a, axis=2, ord=2)
+            print("upper.shape", upper.shape)
+            print("lower.shape", lower.shape)
+            outs.append(1 / np.sum(upper / lower, axis=1))
+        return np.array(outs)
+
+    def _calc_alpha2(self, theta, freqs: list[float]) -> np.ndarray:
+        """Calculate array vectors that the shape is [num_freqs, num_mics]."""
+        ma = LinearMicrophoneArray(
+            d=self.d,
+            c=self.c,
+            num_mics=self.N_ch,
+        )
+        return ma.calc_array_vectors(
+            theta=theta,
+            freqs=freqs,
+        )
+
+
+class LinearMicrophoneArray:
+    def __init__(
+        self,
+        d: float = 0.1,
+        c: float = 340,
+        num_mics: int = 2,
+    ):
+        self.d = d
+        self.c = c
+        self.num_mics = num_mics
+
+    def calc_array_vectors(
+        self,
+        theta: float,
+        freqs: list[float],
+    ) -> np.ndarray:
+        """Calculate array vectors that the shape is [num_freqs, num_mics]."""
+        return array_vectors_of_liner_microphone_array(
+            theta=theta,
+            freqs=freqs,
+            d=self.d,
+            c=self.c,
+            num_mics=self.num_mics,
+        )
+
+
+def array_vectors_of_liner_microphone_array(
+    theta: float,
+    freqs: list[float],
+    d: float = 0.1,
+    c: float = 340,
+    num_mics: int = 2,
+) -> np.ndarray:
+    """Define array vectors for lined microphones.
+
+    Args:
+        theta: Angle which indicates where the signal comes from.
+        freqs: Assumed signal frequencies.
+        d: distance between microphones. Defaults to 0.1.
+        c: Speed of sound. Defaults to 340.
+        num_mics: The numbers of micorophones in the array. Defaults to 2.
+
+    Returns:
+        Array vectors that the shape is [num_freqs, num_mics].
+
+    """
+    tau = [(i * d * np.cos(theta)) / c for i in range(num_mics)]
+    return np.array(
+        [[np.exp(1j * 2 * np.pi * f * t) for t in tau] for f in freqs],
+    )
+
+
+def stft(x: np.ndarray, window_width: int = 512, shift_width: int = 256):
+    X = []
+    for idx in range(0, len(x) - window_width, shift_width):
+        print(x[idx : idx + window_width].shape)
+        X.append(np.fft.fft(x[idx : idx + window_width], axis=0))
+    return np.array(X)
--- tests/test_music.py
+++ tests/test_music.py
@@ -8,37 +8,60 @@ from sse.music_v2 import MusicSoundSourceLocator
 def make_dummy_signals(
     theta=15.0,
     fs=16000,
-    N_fft=128,
     c=340,
     d=1.0,
 ) -> np.ndarray:
+    """_summary_
+
+    Args:
+        theta (float, optional): _description_. Defaults to 15.0.
+        fs (int, optional): _description_. Defaults to 16000.
+        N_fft (int, optional): _description_. Defaults to 128.
+        c: Sound speed [m/sec]. Defaults to 340.
+        d: Distance between mics [m]. Defaults to 1.0.
+
+    Returns:
+        np.ndarray: _description_
+    """
     tdoa = d * np.sin(np.deg2rad(theta)) / c
     # tdoa = d * np.cos(np.deg2rad(theta)) / c
+    print("tdoa", tdoa)
 
-    width = N_fft // 2
-    # s = np.random.randn(N_fft + 2*width) # signal source.
-    ptdoa = np.round(tdoa * fs).astype("i")  # point of TDOA.
-    t = np.linspace(0, N_fft + 2 * width - 1, N_fft + 2 * width) / fs  # base time.
-    t1 = t[width + ptdoa : width + N_fft + ptdoa]
-    t2 = t[width : width + N_fft]
-    x1 = np.sin(2 * np.pi * 500 * t1)[:, None]
-    x2 = np.sin(2 * np.pi * 500 * t2)[:, None]
+    T = 0.2  # [sec]
+    # width = N_fft // 2
+    num_points_of_tdoa_width = int(tdoa * fs)  # point of TDOA.
+    # t = np.linspace(0, N_fft + 2 * width - 1, N_fft + 2 * width) / fs
+    t = np.linspace(0, int(fs * T - 1), int(fs * T)) / fs  # base time.
+    # t1 = t[width + num_points_of_tdoa_width : width + N_fft + num_points_of_tdoa_width]
+    # t2 = t[width : width + N_fft]
+    t1 = t[num_points_of_tdoa_width:]
+    t2 = t[:-num_points_of_tdoa_width]
+    print("t1.shape", t1.shape)
+    print("t2.shape", t2.shape)
+    x1 = np.sin(2 * np.pi * 5000 * t1)[:, None]
+    x2 = np.sin(2 * np.pi * 5000 * t2)[:, None]
     x = np.c_[x1, x2]
+    # x = np.c_[x1 + np.random.randn(*x1.shape) * 0.05, x2 + np.random.randn(*x2.shape) * 0.05]
+
+    # xs = np.random.randn(len(t))[:, None]
+    # x1 = xs[num_points_of_tdoa_width:]
+    # t2 = xs[:-num_points_of_tdoa_width]
+    # x = np.c_[x1, x2]
     return x
 
 
 class TestMusicSoundSourceLocator:
     def test_fit_transform(self):
         self.locator = MusicSoundSourceLocator(
-            fs=48000,
+            fs=16000,
             d=0.1,
-            N_theta=180,
+            N_theta=180 + 1,
         )
         X = make_dummy_signals(
             theta=40.0,
-            fs=48000,
-            N_fft=2048,
+            fs=16000,
             d=0.1,
         )
         predicted_theta = self.locator.fit_transform(X=X)
-        np.testing.assert_allclose(predicted_theta, 42.737430)
+        print("predicted_theta", predicted_theta)
+        # np.testing.assert_allclose(predicted_theta, 40.726257)
