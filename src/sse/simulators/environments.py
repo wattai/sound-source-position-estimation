7638e38b0ea833caec9d6c24356f8711ec8b01cf
wattai <evamark.wattai@gmail.com>
2024-11-12 01:32:24 +0900

    Add Observer

--- src/sse/simulators/environments.py
+++ src/sse/simulators/environments.py
@@ -5,6 +5,7 @@ import abc
 
 from pydantic import BaseModel
 import numpy as np
+import scipy as sp
 import soundfile as sf
 
 
@@ -65,8 +66,8 @@ class Source(BaseSource):
         position: Position3D,
         signal: Signal,
     ):
-        self.signal = signal
         self.position = position
+        self.signal = signal
 
     def ring(self) -> Signal:
         return self.signal
@@ -78,7 +79,7 @@ def file_to_signals(filepath: str) -> list[Signal]:
 
 class BaseSignalGenerator(abc.ABC):
     @abc.abstractmethod
-    def generate(self, sampling_frequency: float) -> Signal:
+    def generate(self, sampling_frequency: float, time_length: float) -> Signal:
         pass
 
 
@@ -87,17 +88,68 @@ class SineSignalGenerator(BaseSignalGenerator):
         self,
         frequency: float,
     ):
-        self.frequeny = frequency
+        self.frequency = frequency
 
-    def generate(self, sampling_frequency: float) -> Signal:
-        return np.sin()
+    def generate(self, sampling_frequency: float, time_length: float) -> Signal:
+        time = make_time_sequence(sampling_frequency, time_length)
+        return Signal(
+            values=list(np.sin(2 * np.pi * self.frequency * time)),
+            sampling_frequency=sampling_frequency,
+        )
+
+
+def make_time_sequence(sampling_frequency: float, time_length: float) -> np.ndarray:
+    return np.linspace(
+        0,
+        int(sampling_frequency * time_length),
+        int(sampling_frequency * time_length) - 1,
+    )
 
 
 class BaseMicrophone(BaseDevice):
-    pass
+    @abc.abstractmethod
+    def record(self, signals: list[Signal]) -> Signal:
+        pass
 
 
-class BaseEnvironment(abc.ABC):
+class Microphone(BaseMicrophone):
+    def __init__(
+        self,
+        position: Position3D,
+        sampling_frequency: float,
+    ):
+        self.position = position
+        self.sampling_frequency = sampling_frequency
+
+    def record(self, signals: list[Signal]) -> Signal:
+        return Signal(
+            values=signals,
+            sampling_frequency=self.sampling_frequency,
+        )
+
+
+def overlap(signals: list[Signal], sampling_frequency: float) -> Signal:
+    resampled = [resample(s, sampling_frequency) for s in signals]
+    len_longest = max(len(s.values) for s in signals)
+    overlapped_array = np.zeros(len_longest)
+    for signal in resampled:
+        overlapped_array[: len(signal.values)] += np.array(signal.values)
+    return Signal(
+        values=list(overlapped_array),
+        sampling_frequency=sampling_frequency,
+    )
+
+
+def resample(signal: Signal, sampling_frequency: float) -> Signal:
+    return sp.signal.resample(
+        signal.values,
+        num=np.ceil(
+            len(signal.values) * (sampling_frequency / signal.sampling_frequency),
+        ),
+    )
+
+
+class BaseMedium(abc.ABC):
     @property
     def temperature(self) -> float:
         return self._temperature
@@ -107,31 +159,41 @@ class BaseEnvironment(abc.ABC):
     def sound_speed(self) -> float:
         pass
 
-    @abc.abstractmethod
-    def ring_sources(self) -> list[Signal]:
-        pass
 
-
-class Air(BaseEnvironment):
+class Air(BaseMedium):
     def __init__(
         self,
-        sources: list[BaseSource],
-        microphones: list[BaseMicrophone],
         temperature: float = 20.0,
     ):
-        self.sources = sources
-        self.microphones = microphones
         self._temperature = temperature
 
     @property
     def sound_speed(self) -> float:
         return 331.3 + 0.6 * self._temperature
 
+
+class BaseObserver(abc.ABC):
+    @abc.abstractmethod
+    def ring_sources(self) -> list[Signal]:
+        pass
+
+
+class Observer(BaseObserver):
+    def __init__(
+        self,
+        sources: list[BaseSource],
+        microphones: list[BaseMicrophone],
+        medium: BaseMedium,
+    ):
+        self.sources = sources
+        self.microphones = microphones
+        self.medium = medium
+
     def ring_sources(self) -> list[Signal]:
         return calc_received_signals(
             self.sources,
             self.microphones,
-            sound_speed=self.sound_speed,
+            sound_speed=self.medium.sound_speed,
         )
 
 
@@ -140,4 +202,24 @@ def calc_received_signals(
     microphones: list[BaseMicrophone],
     sound_speed: float,
 ) -> list[Signal]:
-    pass
+    return [
+        mic.record(
+            [
+                delay(
+                    signal=source.ring(),
+                    distance=mic.position.distance(source.position),
+                    sound_speed=sound_speed,
+                )
+                for source in sources
+            ],
+        )
+        for mic in microphones
+    ]
+
+
+def delay(signal: Signal, distance: float, sound_speed: float) -> Signal:
+    num_delay_points = signal.sampling_frequency * (distance / sound_speed)
+    return Signal(
+        values=np.pad(signal.values[num_delay_points:], ((0, num_delay_points))),
+        sampling_frequency=signal.sampling_frequency,
+    )
